a94a096c1fbcb064a05e99dd4386e095a8c3cd95
Jakub Jakeš <91247920+JakesJakub@users.noreply.github.com>
2022-04-10 19:00:15 +0200

    Počáteční soubory projektu MakeCode

---
+++ main.py
@@ -0,0 +1,102 @@
+led.plot_brightness(0 + 2, 0 + 2, 100)
+led.plot(0, 0)
+led.plot(1, 1)
+
+def on_button_pressed_a():
+        basic.clear_screen()
+        led.plot_brightness(0 + 2, 0 + 2, 100)
+        led.plot(0, 2)
+        led.plot(1,2)
+        basic.pause(500)
+
+        basic.clear_screen()
+        led.plot_brightness(0 + 2, 0 + 2, 100)
+        led.plot(0, 4)
+        led.plot(1,3)
+        basic.pause(500)
+
+        basic.clear_screen()
+        led.plot_brightness(0 + 2, 0 + 2, 100)
+        led.plot(2, 4)
+        led.plot(2, 3)
+        basic.pause(500)
+
+        basic.clear_screen()
+        led.plot_brightness(0 + 2, 0 + 2, 100)
+        led.plot(4, 4)
+        led.plot(3, 3)
+        basic.pause(500)
+
+        basic.clear_screen()
+        led.plot_brightness(0 + 2, 0 + 2, 100)
+        led.plot(4, 2)
+        led.plot(3, 2)
+        basic.pause(500)
+
+        basic.clear_screen()
+        led.plot_brightness(0 + 2, 0 + 2, 100)
+        led.plot(4, 0)
+        led.plot(3, 1)
+        basic.pause(500)
+
+        basic.clear_screen()
+        led.plot_brightness(0 + 2, 0 + 2, 100)
+        led.plot(2, 0)
+        led.plot(2, 1)
+        basic.pause(500)
+
+        basic.clear_screen()
+        led.plot_brightness(0 + 2, 0 + 2, 100)
+        led.plot(0, 0)
+        led.plot(1, 1)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+
+        basic.clear_screen()
+        led.plot_brightness(0 + 2, 0 + 2, 100)
+        led.plot(2, 0)
+        led.plot(2, 1)
+        basic.pause(500)
+
+        basic.clear_screen()
+        led.plot_brightness(0 + 2, 0 + 2, 100)
+        led.plot(4, 0)
+        led.plot(3, 1)
+        basic.pause(500)
+
+        basic.clear_screen()
+        led.plot_brightness(0 + 2, 0 + 2, 100)
+        led.plot(4, 2)
+        led.plot(3, 2)
+        basic.pause(500)
+
+        basic.clear_screen()
+        led.plot_brightness(0 + 2, 0 + 2, 100)
+        led.plot(4, 4)
+        led.plot(3, 3)
+        basic.pause(500)
+
+        basic.clear_screen()
+        led.plot_brightness(0 + 2, 0 + 2, 100)
+        led.plot(2, 4)
+        led.plot(2, 3)
+        basic.pause(500)
+
+        basic.clear_screen()
+        led.plot_brightness(0 + 2, 0 + 2, 100)
+        led.plot(0, 4)
+        led.plot(1,3)
+        basic.pause(500)
+
+        basic.clear_screen()
+        led.plot_brightness(0 + 2, 0 + 2, 100)
+        led.plot(0, 2)
+        led.plot(1, 2)
+        basic.pause(500)
+
+        basic.clear_screen()
+        led.plot_brightness(0 + 2, 0 + 2, 100)
+        led.plot(0, 0)
+        led.plot(1, 1)
+input.on_button_pressed(Button.B, on_button_pressed_b)
